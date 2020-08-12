8a18ee616979f524a06e2f225874bd8234af9737
progjs <js568@ajou.ac.kr>
2020-08-12 17:13:13 +0900

    'backup'

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\jinsilpython\recommend_movie\venv"
+$env:VIRTUAL_ENV="C:\mypython\recommend_movie\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
