c09208b2b2b813cdf145e42bd0942e80c2362b1a
progjs <js568@ajou.ac.kr>
2020-08-12 10:12:46 +0900

    bootstrap template 적용

--- django_movie/movieapp/urls.py
+++ django_movie/movieapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='index'),
-
+    path('', views.about, name='about'),
+    path('', views.contact, name='contact'),
+    path('', views.services, name='services'),
+    path('', views.works, name='works'),
+    path('work_single/', views.work_single, name='work_single'),
 ]
--- django_movie/movieapp/views.py
+++ django_movie/movieapp/views.py
@@ -6,3 +6,23 @@ from django.shortcuts import render
 def base(request):
     name = '영화'
     return render(request, 'movieapp/index.html')
+
+def contact(request):
+    name = '영화'
+    return render(request, 'movieapp/contact.html')
+
+def about(request):
+    name = '영화'
+    return render(request, 'movieapp/about.html')
+
+def services(request):
+    name = '영화'
+    return render(request, 'movieapp/services.html')
+
+def works(request):
+    name = '영화'
+    return render(request, 'movieapp/works.html')
+
+def work_single(request):
+    name = '영화'
+    return render(request, 'movieapp/work-single.html')
