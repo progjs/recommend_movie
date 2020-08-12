e3b52aefa4bb4e0d66072d303a1eb0ec2ccd4b33
progjs <js568@ajou.ac.kr>
2020-08-12 11:54:59 +0900

    가상환경

--- django_movie/movieapp/urls.py
+++ django_movie/movieapp/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.base, name='index'),
-    path('', views.about, name='about'),
-    path('', views.contact, name='contact'),
-    path('', views.services, name='services'),
-    path('', views.works, name='works'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('work/', views.works, name='works'),
     path('work_single/', views.work_single, name='work_single'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
