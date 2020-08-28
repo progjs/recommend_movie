afe940d1ca4de474c3c19d5e047f672d63dc455e
yoonyoung <dbsydd@gmail.com>
2020-08-28 16:17:14 +0900

    로그인 후 redirect 처리

--- django_movie/movieapp/urls.py
+++ django_movie/movieapp/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     ## 추가기능
     path('movie_detail/<int:pk>/', views.movie_detail, name='movie_detail'),
     path('movie_detail/wishlist/', views.add_wishlist, name='add_wishlist'),
-    path('search', views.search_movie, name='search_movie'),
+    path('search/', views.search_movie, name='search_movie'),
     # path('genre/', views.genre_filter, name='genre_filter'),
-    path('wishlist/', views.find_wishlist, name='find_wishlist'),
+    path('wishlist/', views.show_wishlist, name='show_wishlist'),
 
     ## 댓글 CRUD
     path('comment/<int:pk>/', views.add_comment, name='add_comment'),
