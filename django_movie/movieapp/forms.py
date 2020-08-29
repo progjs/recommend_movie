dadc6a207759ddf5325b71254b98a24a233147d6
yoonyoung <dbsydd@gmail.com>
2020-08-29 14:15:29 +0900

    temp

--- django_movie/movieapp/forms.py
+++ django_movie/movieapp/forms.py
@@ -11,11 +11,23 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'email', 'username', 'password')
+        labels = {
+            "first_name": "이름",
+            "email": "이메일",
+            "username": "아이디",
+            "password": "비밀번호"
+        }
 
 
 class UserDetailForm(forms.ModelForm):
     class Meta:
         model = UserDetail
         fields = ('password2', 'sex', 'birth', 'favorite_genre')
+        labels = {
+            "password2": "비밀번호 확인",
+            "sex": "성별",
+            "birth": "생년월일",
+            "favorite_genre": "좋아하는 영화장르"
+        }
 
 
